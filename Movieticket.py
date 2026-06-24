31faa2032914700b80602a24d2c9f75997f59460
Steven Martsching <stout105228@gmail.com>
2026-06-24 13:04:46 -0700

    uploading test file to repo movie ticket

---
+++ Movieticket.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age: "))
+student = input("Are you a student? (Y/N): ")
+ 
+if age < 13:
+    price = 8
+elif age >= 65:
+    price = 10
+else:
+    price = 12
+ 
+if student == "Y":
+    price = price - 2
+ 
+print("Ticket price: $", price)
